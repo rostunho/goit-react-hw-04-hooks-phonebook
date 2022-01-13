a1826b1a948c6bcf952d522c93aa9d7d083d7097
Rostyk Podolets <ros.podolets@gmail.com>
2022-01-13 15:35:31 +0200

    lazy initialization added

--- src/App.js
+++ src/App.js
@@ -7,26 +7,28 @@ import ContactForm from 'components/ContactForm/ContactForm';
 import Title from 'components/Title/Title';
 import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
+import defaultContacts from './database/default-contacts.json';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const unparsedContacts = window.localStorage.getItem('contacts');
+    const savedContacts = JSON.parse(unparsedContacts);
+
+    return savedContacts ?? defaultContacts;
+  });
+
   const [newContact, setNewContact] = useState({
     name: '',
     number: '',
   });
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const unparsedContacts = window.localStorage.getItem('contacts');
-    const savedContacts = JSON.parse(unparsedContacts);
+  // useEffect(() => {
+  //   const unparsedContacts = window.localStorage.getItem('contacts');
+  //   const savedContacts = JSON.parse(unparsedContacts);
 
-    savedContacts && setContacts(savedContacts);
-  }, []);
+  //   savedContacts && setContacts(savedContacts);
+  // }, []);
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -71,6 +73,12 @@ function App() {
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter),
   );
+
+  // const filteredContacts = useMemo(() => {
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(normalizedFilter),
+  //   );
+  // }, [contacts, normalizedFilter]);
 
   return (
     <>
